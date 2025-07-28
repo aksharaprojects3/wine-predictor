d396e57c7984fe8639d93434e097f75c29bfac29
aksharaprojects3 <aksharasaraswat3@gmail.com>
2025-07-28 11:00:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load("wine_model.pkl")
+
+@app.route("/")
+def home():
+    return "Wine Prediction API is running!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({"prediction": prediction.tolist()})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
